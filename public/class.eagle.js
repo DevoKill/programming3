0b3f859eeba7ad68c8c8b2874c81333dc5be3737
Your Name <you@example.com>
2018-04-13 17:25:03 +0400

    version 1.2.1

--- public/class.eagle.js
+++ public/class.eagle.js
@@ -1,35 +1,35 @@
-class Eagle extends Creature{
+class Eagle extends Creature {
     constructor(x, y) {
         super(x, y)
         this.directions = [];
     }
-    cordinates(){
-        if(this.b == 2){
-            for(var i = n - 1; i >= 0; i--){
+    cordinates() {
+        if (this.b == 2) {
+            for (var i = n - 1; i >= 0; i--) {
                 var d = [];
-                d.push(this.y+i, this.x-i);
+                d.push(this.y + i, this.x - i);
                 this.directions.push(d);
             }
         }
-        if(this.b == 1){
-            for(var i = 0; i <= n-1; i++){
+        if (this.b == 1) {
+            for (var i = 0; i <= n - 1; i++) {
                 var d = [];
-                d.push(this.y+i, this.x+i);
+                d.push(this.y + i, this.x + i);
                 this.directions.push(d);
             }
         }
     }
-    cho(ch) {cd
+    cho(ch) {
         this.cordinates();
         return super.cho(ch);
     }
 
     utel(s) {
         this.b = s;
-        if(wolves.length >= 5){
+        if (wolves.length >= 5) {
             var norVandak = random(this.cho(3));
         }
-        if(sheeps.length >= 14){
+        if (sheeps.length >= 14) {
             var norVandak2 = random(this.cho(2));
         }
         if (norVandak) {
@@ -44,7 +44,7 @@ class Eagle extends Creature{
             this.x = norVandak[0];
             this.y = norVandak[1];
         }
-        else if(norVandak2) {
+        else if (norVandak2) {
             matrix[norVandak2[1]][norVandak2[0]] = 4;
             matrix[this.y][this.x] = 0;
             for (var i in sheeps) {
